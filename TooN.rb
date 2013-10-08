882ae521aed1f0feb15e67fa98c714c499bb83e9
Randolph Voorhies <rand.voorhies@gmail.com>
2013-10-08 11:33:49 -0700

    Adding home pages

--- TooN.rb
+++ TooN.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Toon < Formula
-  homepage ''
+  homepage 'http://www.edwardrosten.com/cvd/toon.html'
   url 'https://github.com/edrosten/TooN/archive/TOON_2.1.zip'
   sha1 'f1bc7c2e841c75f590e926cd43dcc76f9bfe4e47'
   head 'https://github.com/edrosten/TooN.git'
--- libcvd.rb
+++ libcvd.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Libcvd < Formula
-  homepage ''
+  homepage 'http://www.edwardrosten.com/cvd/'
   url 'https://github.com/edrosten/libcvd/archive/RELEASE_20121025.2.zip'
   sha1 '4e8ad5492060e53f2a30d4abd9a19644a097f5fb'
 
